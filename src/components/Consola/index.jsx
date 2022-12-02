7722733d41a14fa26a5c773b571fd3cf1f0aa7f1
Daniel Marcel <daniel.marcel@inacapmail.cl>
2022-12-02 02:04:18 -0300

    Estilo de consola

--- src/components/Consola/index.jsx
+++ src/components/Consola/index.jsx
@@ -1,44 +1,107 @@
 import Footer from "../Footer";
-import Form from 'react-bootstrap/Form';
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import React, { useState } from 'react';
+import Form from "react-bootstrap/Form";
+import FloatingLabel from "react-bootstrap/FloatingLabel";
+import React, { useState } from "react";
 import { evaludateCode } from "../../api/code";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
 const Consola = () => {
-    const [result, setResult] = useState("");
-    const [code, setCode] = useState("");
+  const [result, setResult] = useState("");
+  const [code, setCode] = useState("");
 
+  const handleSubmit = async () => {
+    try {
+      const evaluated = await evaludateCode(code);
+      setResult(evaluated.result);
+    } catch (err) {
+      setResult(err.message);
+    }
+  };
 
-    const handleSubmit = async () => {
-        try {
-            const evaluated = await evaludateCode(code);
-            setResult(evaluated.result);
-        } catch (err) {
-            setResult(err.message);
-        }
-
-    };
-
-
-    return (
-        <div>
-            <FloatingLabel controlId="floatingTextarea2" label="Comments">
-                <Form.Control
-                    as="textarea"
-                    onChange={(e) => setCode(e.target.value)}
-                    placeholder="Insert code here"
-                    style={{ height: '100px' }}
-                />
-            </FloatingLabel>
-            <Button variant="primary" onClick={handleSubmit}>Primary</Button>
-            <div className="result">
-                <h3>Result</h3>
-                <p>{result}</p>
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div>
+      <FloatingLabel controlId="floatingTextarea2" className="container">
+        <Form.Control
+          as="textarea"
+          onChange={(e) => setCode(e.target.value)}
+          placeholder="Insert code here"
+          style={{
+            height: "250px",
+            width: "100%",
+            marginTop: "20px",
+            marginBottom: "20px",
+            backgroundColor: "#1e1e1e",
+            color: "white",
+            border: "1px solid #1e1e1e",
+            borderRadius: "5px",
+            fontSize: "20px",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            padding: "10px",
+            resize: "none",
+            outline: "none",
+            boxShadow: "0 0 10px #1e1e1e",
+          }}
+        />
+      </FloatingLabel>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Button
+          variant="primary"
+          onClick={handleSubmit}
+          style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            backgroundColor: "#1e1e1e",
+            color: "white",
+            border: "1px solid #1e1e1e",
+            borderRadius: "5px",
+            fontSize: "20px",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            padding: "10px",
+            resize: "none",
+            outline: "none",
+            boxShadow: "0 0 10px #1e1e1e",
+          }}
+        >
+          Ver Resultado
+        </Button>
+      </div>
+      <div
+        className="result"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <h3>Resultado</h3>
+        <p style={{
+            marginTop: "20px",
+            marginBottom: "20px",
+            backgroundColor: "#1e1e1e",
+            color: "white",
+            border: "1px solid #1e1e1e",
+            borderRadius: "5px",
+            fontSize: "20px",
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            padding: "10px",
+            resize: "none",
+            outline: "none",
+            boxShadow: "0 0 10px #1e1e1e",
+        }}>{result}</p>
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
 export default Consola;
